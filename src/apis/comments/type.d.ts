d0015f6c2e05b00f008719f0729213270e867e69
chaewon <devchae10@gmail.com>
2026-04-20 20:59:20 +0900

    feat: comments api 작업

---
+++ src/apis/comments/type.d.ts
@@ -0,0 +1,36 @@
+export interface CreateCommentRequest {
+	content: string;
+	cardId: number;
+	columnId: number;
+	dashboardId: number;
+}
+
+export interface UpdateCommentRequest {
+	content: string;
+}
+
+export interface Author {
+	profileImageUrl: string | null;
+	nickname: string;
+	id: number;
+}
+
+export interface Comment {
+	id: number;
+	content: string;
+	createdAt: string;
+	updatedAt: string;
+	cardId: number;
+	author: Author;
+}
+
+export interface GetCommentListRequest {
+	size?: number;
+	cursorId?: number;
+	cardId: number;
+}
+
+export interface GetCommentListResponse {
+	cursorId: number | null;
+	comments: Comment[];
+}
