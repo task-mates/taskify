aaa20e94d40e71355517c46b6d1ad303040b5a89
pho9902 <bagh9902@gmail.com>
2026-04-20 17:25:56 +0900

    feat: members API 추가

---
+++ src/apis/members/index.ts
@@ -0,0 +1,14 @@
+import instance from "@/src/apis/instance";
+import { GetMemberListResponse } from "@/src/apis/members/type";
+
+export const membersApi = {
+  getList: async (dashboardId: number, page?: number, size?: number) => {
+    const { data } = await instance.get<GetMemberListResponse>("/members", {
+      params: { dashboardId, page, size },
+    });
+    return data;
+  },
+  delete: async (memberId: number) => {
+    await instance.delete(`/members/${memberId}`);
+  },
+};
