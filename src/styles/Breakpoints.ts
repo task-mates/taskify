d21e171ebc521d0bab04b71baa2b7f0304428001
chaewon <devchae10@gmail.com>
2026-04-21 21:21:26 +0900

    feat: 반응형 height 분기점 추가

--- src/styles/Breakpoints.ts
+++ src/styles/Breakpoints.ts
@@ -1,4 +1,8 @@
 export const DEVICE = {
   tablet: '(max-width: 1199px)',
   mobile: '(max-width: 767px)',
+
+  heightMd: '(max-height: 800px)',
+  heightSm: '(max-height: 680px)',
+  heightXs: '(max-height: 540px)',
 };
