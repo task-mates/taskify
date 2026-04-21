4b0968360389fbdc900d41c210ab4ff75eecb685
chaewon <devchae10@gmail.com>
2026-04-22 00:16:55 +0900

    feat: height 분기점 변경

--- src/styles/Breakpoints.ts
+++ src/styles/Breakpoints.ts
@@ -2,7 +2,7 @@ export const DEVICE = {
   tablet: '(max-width: 1199px)',
   mobile: '(max-width: 767px)',
 
-  heightMd: '(max-height: 800px)',
+  heightMd: '(max-height: 850px)',
   heightSm: '(max-height: 680px)',
   heightXs: '(max-height: 540px)',
 };
