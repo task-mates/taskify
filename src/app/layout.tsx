1316dd83ce9eea7fd3dc11ec7597d95d5ec7cb09
pho9902 <bagh9902@gmail.com>
2026-04-21 13:51:19 +0900

    fix: 코드리뷰 반영

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,25 @@
 import type { Metadata } from "next";
 import StyledComponentsRegistry from "./registry";
+import GlobalStyle from "@/src/styles/GlobalStyle";
 
 export const metadata: Metadata = {
-	title: "Taskify",
-	description: "Task management application",
+  title: "Taskify",
+  description: "Task management application",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="ko">
-			<body>
-				<StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="ko">
+      <body>
+        <StyledComponentsRegistry>
+          <GlobalStyle />
+          {children}
+        </StyledComponentsRegistry>
+      </body>
+    </html>
+  );
 }
