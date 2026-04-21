b7c516069ffd7a872346f101b4c9a1323a020460
chaewon <devchae10@gmail.com>
2026-04-21 16:33:45 +0900

    chore: style 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,12 +1,14 @@
-'use client';
+"use client";
 
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   *, *::before, *::after {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font:inherit;
+    color:inherit;
   }
 
   html, body {
@@ -20,6 +22,20 @@ const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+
+  img, picture, video, svg{
+    display: block;
+    max-width: 100%;
+  }
+
+  button {
+    background:none;
+    border:0;
+    cursor:pointer;
+    }
+  a {
+    text-decoration:none;
+    }
 `;
 
 export default GlobalStyle;
