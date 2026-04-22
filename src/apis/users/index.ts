2926d80ed52b3e2cc1b309537232d6fa0d1a6ea2
ekdmskwon <dadax0715@gamil.com>
2026-04-22 13:50:42 +0900

    fix: teamId 파라미터 제거

--- src/apis/users/index.ts
+++ src/apis/users/index.ts
@@ -7,30 +7,27 @@ import {
 } from "@/src/apis/users/type";
 
 export const usersApi = {
-  signUp: async (teamId: string | number, body: SignUpRequest) => {
-    const { data } = await instance.post<User>(`/${teamId}/users`, body);
+  signUp: async (body: SignUpRequest) => {
+    const { data } = await instance.post<User>("/users", body);
     return data;
   },
 
-  getMe: async (teamId: string | number) => {
-    const { data } = await instance.get<User>(`/${teamId}/users/me`);
+  getMe: async () => {
+    const { data } = await instance.get<User>("/users/me");
     return data;
   },
 
-  updateMe: async (
-    teamId: string | number,
-    body: UpdateMyInfoRequest,
-  ) => {
-    const { data } = await instance.put<User>(`/${teamId}/users/me`, body);
+  updateMe: async (body: UpdateMyInfoRequest) => {
+    const { data } = await instance.put<User>("/users/me", body);
     return data;
   },
 
-  uploadMyImage: async (teamId: string | number, image: File) => {
+  uploadMyImage: async (image: File) => {
     const formData = new FormData();
     formData.append("image", image);
 
     const { data } = await instance.post<UploadProfileImageResponse>(
-      `/${teamId}/users/me/image`,
+      "/users/me/image",
       formData,
     );
     return data;
