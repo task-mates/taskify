c403cdfc801bd7dff3f847ff781d9348706a816e
김윰 <kimyumin71@gmail.com>
2026-04-23 16:01:51 +0900

    fix: 채원님 코드리뷰 반영

--- src/apis/auth/index.ts
+++ src/apis/auth/index.ts
@@ -6,11 +6,10 @@ import {
   } from "@/src/apis/auth/type";
 
 export const postLogin = async (data: LoginRequest): Promise<LoginResponse> => {
-  const response = await instance.post<LoginResponse>(`/login`, data);
+  const response = await instance.post<LoginResponse>(`/auth/login`, data);
   return response.data;
 };
 
-export const putResetPassword = async (data: ResetPasswordRequest): Promise<void> => {
-  const response = await instance.put(`/password`, data);
-  return response.data;
+export const putPassword = async (data: ResetPasswordRequest): Promise<void> => {
+  await instance.put('/auth/password', data);
 };
