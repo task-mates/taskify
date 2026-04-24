d2107173d3fbb4a756c510759c41088ddaf99c52
김윰 <kimyumin71@gmail.com>
2026-04-24 15:20:46 +0900

    fix: 제미나이 코드리뷰 반영

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,27 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
-  
+
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not || []), /url/] },
+        use: ["@svgr/webpack"],
+      },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
   },
--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -5,3 +5,7 @@ declare module "*.svg" {
   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
 }
+declare module "*.svg?url" {
+  const content: any;
+  export default content;
+}
