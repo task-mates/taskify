dea36a6d85ad85ea216e9ec5931ce1b3dd48352e
chahyunlee <chahyun023@gmail.com>
2026-04-24 11:42:20 +0900

    fix: 코드리뷰 반영

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,5 @@
-export default function PublicLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <>{children}</>;
+import AppShell from '@/src/components/layout/AppShell';
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return <AppShell>{children}</AppShell>;
 }
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,7 @@
-import AppShell from '@/src/components/layout/AppShell';
-
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return <AppShell>{children}</AppShell>;
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
 }
