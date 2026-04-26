b4353484526e5b19e89dff149f93b7edb9ae3cfb
chaewon <devchae10@gmail.com>
2026-04-26 13:48:48 +0900

    refactor: ReactNode 표기 원복

--- src/app/registry.tsx
+++ src/app/registry.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { ReactNode, useState } from 'react';
+import { useState } from 'react';
 import { useServerInsertedHTML } from 'next/navigation';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
 export default function StyledComponentsRegistry({
   children,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
 }) {
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
 
