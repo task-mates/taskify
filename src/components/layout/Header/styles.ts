026795fc2e9491a63db793aa64d2caef9d37c1b0
pho9902 <bagh9902@gmail.com>
2026-04-27 14:40:59 +0900

    feat: 헤더 컴포넌트 구조 추가 및 렌더 위치 설정

---
+++ src/components/layout/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.header`
+  position: sticky;
+  top: 0;
+  width: 100%;
+  height: 70px;
+  background: var(--color-red);
+  z-index: 100;
+`;
