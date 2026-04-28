16fe09871a6ba7097ccc06f6a6df4e3cb2b07733
김윰 <kimyumin71@gmail.com>
2026-04-28 14:36:42 +0900

    fix: 오류 수정

--- next.config.ts
+++ next.config.ts
@@ -7,37 +7,40 @@ const nextConfig: NextConfig = {
   },
 
   webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule: RuleSetRule|'...') =>
-       rule !== '...' && (rule.test as RegExp)?.test?.('.svg')
-    );
+    const fileLoaderRule = config.module.rules.find((rule: RuleSetRule | '...') =>
+      rule !== '...' && (rule.test as RegExp)?.test?.('.svg')
+    ) as RuleSetRule;
 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not || []), /url/] },
-        use: ["@svgr/webpack"],
-      },
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
+    if (fileLoaderRule) {
+      config.module.rules.push(
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/,
+        },
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...((fileLoaderRule.resourceQuery as any)?.not || []), /url/] },
+          use: ["@svgr/webpack"],
+        },
+      );
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
 
     return config;
   },
-};
 
-turbopack: {
-  rules: {
-    '.svg': {
-      loaders: ['@svgr/webpack'],
-      as: '.js',
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
     },
   },
-},
+};
 
 export default nextConfig;
