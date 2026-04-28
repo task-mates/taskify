f299343f57595658f79e2def9a685ae202ae3913
김윰 <kimyumin71@gmail.com>
2026-04-28 14:41:45 +0900

    fix: 터보팩 설정 위치 수정

--- next.config.ts
+++ next.config.ts
@@ -6,40 +6,39 @@ const nextConfig: NextConfig = {
     styledComponents: true,
   },
 
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule: RuleSetRule | '...') =>
-      rule !== '...' && (rule.test as RegExp)?.test?.('.svg')
-    ) as RuleSetRule;
-
-    if (fileLoaderRule) {
-      config.module.rules.push(
-        {
-          ...fileLoaderRule,
-          test: /\.svg$/i,
-          resourceQuery: /url/,
-        },
-        {
-          test: /\.svg$/i,
-          issuer: fileLoaderRule.issuer,
-          resourceQuery: { not: [...((fileLoaderRule.resourceQuery as any)?.not || []), /url/] },
-          use: ["@svgr/webpack"],
-        },
-      );
-      fileLoaderRule.exclude = /\.svg$/i;
-    }
-
-    return config;
-  },
-
   experimental: {
     turbo: {
       rules: {
-        '*.svg': {
+        '.svg': {
           loaders: ['@svgr/webpack'],
-          as: '*.js',
+          as: 'js',
         },
       },
     },
+  },
+
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: RuleSetRule | '...') =>
+      rule !== '...' && (rule.test as RegExp)?.test?.('.svg')
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not || []), /url/] },
+        use: ["@svgr/webpack"],
+      },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
   },
 };
 
