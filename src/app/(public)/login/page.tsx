bb17faeebfec88a85ce58d1316922c6959c6ecf9
ekdmskwon <dadax0715@gamil.com>
2026-04-29 14:12:09 +0900

    style: 로그인, faq 페이지 싱글쿼트로 통일

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -10,7 +10,7 @@ import {
   AUTH_VALIDATION_MESSAGES,
   isValidEmail,
   isValidPassword,
-} from '@/src/utils/authValidation';
+} from '../../../utils/authValidation';
 import * as S from './styles';
 import Link from 'next/link';
 
@@ -158,11 +158,11 @@ export default function LoginPage() {
   return (
     <S.Container>
       <S.FormSection>
-        <Link href="/" aria-label="메인 페이지 이동">
+        <Link href='/' aria-label='메인 페이지 이동'>
           <S.LogoWrapper>
             <Image
-              src="/images/icon-logo.svg"
-              alt="Taskify 로고"
+              src='/images/icon-logo.svg'
+              alt='Taskify 로고'
               fill
               priority
             />
@@ -170,11 +170,11 @@ export default function LoginPage() {
         </Link>
 
         <S.LoginForm onSubmit={handleSubmit}>
-          <S.Label htmlFor="email">이메일</S.Label>
+          <S.Label htmlFor='email'>이메일</S.Label>
           {/* 인풋 공통 컴포넌트 사용 예정 */}
           <S.TextInput
-            id="email"
-            type="email"
+            id='email'
+            type='email'
             $hasError={Boolean(errors.email)}
             value={email}
             onChange={(e) => {
@@ -194,15 +194,15 @@ export default function LoginPage() {
               }
             }}
             onBlur={handleEmailBlur}
-            placeholder="이메일을 입력해주세요"
+            placeholder='이메일을 입력해주세요'
           />
           <S.ErrorText>{errors.email || ' '}</S.ErrorText>
 
-          <S.Label htmlFor="password">비밀번호</S.Label>
+          <S.Label htmlFor='password'>비밀번호</S.Label>
           <S.PasswordField>
             {/* 인풋 공통 컴포넌트 사용 예정 */}
             <S.PasswordInput
-              id="password"
+              id='password'
               type={isPasswordVisible ? 'text' : 'password'}
               $hasError={Boolean(errors.password)}
               value={password}
@@ -223,11 +223,11 @@ export default function LoginPage() {
                 }
               }}
               onBlur={handlePasswordBlur}
-              placeholder="비밀번호를 입력해주세요"
+              placeholder='비밀번호를 입력해주세요'
             />
             {/* 눈 아이콘 클릭 시 비밀번호 보기/숨기기 */}
             <S.TogglePasswordButton
-              type="button"
+              type='button'
               onClick={() => setIsPasswordVisible((prev) => !prev)}
               aria-label={
                 isPasswordVisible ? '비밀번호 숨기기' : '비밀번호 보기'
@@ -239,7 +239,7 @@ export default function LoginPage() {
                     ? '/images/password-eye-on.svg'
                     : '/images/password-eye-off.svg'
                 }
-                alt=""
+                alt=''
                 width={20}
                 height={20}
               />
@@ -250,14 +250,14 @@ export default function LoginPage() {
           {errors.common && <S.ErrorText>{errors.common}</S.ErrorText>}
 
           {/* 버튼 공통 컴포넌트 사용 예정 */}
-          <S.LoginButton type="submit" disabled={isLoginButtonDisabled}>
+          <S.LoginButton type='submit' disabled={isLoginButtonDisabled}>
             {isLoading ? '로그인 중' : '로그인'}
           </S.LoginButton>
         </S.LoginForm>
 
         <S.SignupRow>
           <S.HelperText>아직 회원이 아니신가요?</S.HelperText>
-          <S.SignupLink href="/signup">회원가입하기</S.SignupLink>
+          <S.SignupLink href='/signup'>회원가입하기</S.SignupLink>
         </S.SignupRow>
       </S.FormSection>
     </S.Container>
