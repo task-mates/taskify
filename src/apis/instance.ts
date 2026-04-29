f2572d7ae8561fb5b6472ccf587f9bcefb5afae8
chahyunlee <chahyun023@gmail.com>
2026-04-29 11:36:44 +0900

    fix: 테스트용 instance 헤더 제거

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -4,9 +4,6 @@ import { getAccessToken } from '@/src/utils/authTokenStorage';
 const instance: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 5000,
-  headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NjcwNSwidGVhbUlkIjoiMjMtNCIsImlhdCI6MTc3NzQyNjM0MywiaXNzIjoic3AtdGFza2lmeSJ9.6CRZmL1iPf_aEChAKcoHTAdeK-b0d-37H6lt54y9Cms`,
-  },
 });
 
 instance.interceptors.request.use((config) => {
