5978449bb6815ba7e509611b76804a4f98018cb3
chahyunlee <chahyun023@gmail.com>
2026-04-29 11:20:26 +0900

    refactor: 대시보드 api 구조 개선

--- src/apis/dashboards/index.ts
+++ src/apis/dashboards/index.ts
@@ -7,41 +7,37 @@ import {
   UpdateDashboardRequest,
 } from '@/src/apis/dashboards/type';
 
-export const dashboardsApi = {
-  create: async (body: CreateDashboardRequest) => {
-    const { data } = await instance.post<Dashboard>('/dashboards', body);
-    return data;
-  },
+export const createDashboard = async (body: CreateDashboardRequest) => {
+  const { data } = await instance.post<Dashboard>('/dashboards', body);
+  return data;
+};
 
-  getById: async (dashboardId: number) => {
-    const { data } = await instance.get<Dashboard>(
-      `/dashboards/${dashboardId}`
-    );
-    return data;
-  },
+export const getDashboardById = async (dashboardId: number) => {
+  const { data } = await instance.get<Dashboard>(`/dashboards/${dashboardId}`);
+  return data;
+};
 
-  getList: async (params: GetDashboardListRequest) => {
-    const { data } = await instance.get<GetDashboardListResponse>(
-      '/dashboards',
-      {
-        params: {
-          navigationMethod: 'infiniteScroll',
-          ...params,
-        },
-      }
-    );
-    return data;
-  },
+export const getDashboardList = async (params: GetDashboardListRequest) => {
+  const { data } = await instance.get<GetDashboardListResponse>('/dashboards', {
+    params: {
+      navigationMethod: 'infiniteScroll',
+      ...params,
+    },
+  });
+  return data;
+};
 
-  update: async (dashboardId: number, body: UpdateDashboardRequest) => {
-    const { data } = await instance.put<Dashboard>(
-      `/dashboards/${dashboardId}`,
-      body
-    );
-    return data;
-  },
+export const updateDashboard = async (
+  dashboardId: number,
+  body: UpdateDashboardRequest
+) => {
+  const { data } = await instance.put<Dashboard>(
+    `/dashboards/${dashboardId}`,
+    body
+  );
+  return data;
+};
 
-  remove: async (dashboardId: number) => {
-    await instance.delete(`/dashboards/${dashboardId}`);
-  },
+export const removeDashboard = async (dashboardId: number) => {
+  await instance.delete(`/dashboards/${dashboardId}`);
 };
