0423eabab71d8d601061a24774e38a4a56eaa8f8
pho9902 <bagh9902@gmail.com>
2026-05-04 15:40:55 +0900

    refactor: 하드코딩 색상값 변경, SSR 환경 대응(getComputedStyle)

--- src/components/layout/AppHeader/styles.ts
+++ src/components/layout/AppHeader/styles.ts
@@ -7,8 +7,8 @@ export const Wrapper = styled.header`
   width: 100%;
   height: 60px;
   padding: 0 24px;
-  background: #f8f9fb;
-  border-bottom: 1px solid #e0e0e0;
+  background: var(--color-gray-100);
+  border-bottom: 1px solid var(--color-gray-300);
   z-index: 100;
 
   display: flex;
