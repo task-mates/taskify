5491af5b44ba5f78a47d3606268d7e784d8c1578
pho9902 <bagh9902@gmail.com>
2026-05-04 14:46:50 +0900

    feat: 앱 헤더 프로필 영역 구현

--- src/components/layout/AppHeader/index.tsx
+++ src/components/layout/AppHeader/index.tsx
@@ -1,6 +1,11 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { usersApi } from '@/src/apis/users';
+import type { User } from '@/src/apis/users/type';
+import { getProfileColorByNickname } from '@/src/utils/profileColor';
 import * as S from './styles';
 import type { AppHeaderProps } from '@/src/components/layout/AppHeader/type';
 
@@ -10,10 +15,19 @@ export default function AppHeader({
   createdByMe,
 }: AppHeaderProps) {
   const pathname = usePathname();
+  const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
 
   const isMyDashboard = pathname === '/mydashboard';
   const isOwnedDashboard = !!dashboardId && createdByMe;
   const isInvitedDashboard = !!dashboardId && !createdByMe;
+
+  useEffect(() => {
+    usersApi
+      .getMe()
+      .then(setUser)
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <S.Wrapper>
@@ -25,9 +39,34 @@ export default function AppHeader({
         ☰
       </S.HamburgerButton>
 
-      {isMyDashboard && <>{/* TODO: 마이 대시보드 헤더 콘텐츠 */}</>}
-      {isOwnedDashboard && <>{/* TODO: 내가 만든 대시보드 헤더 콘텐츠 */}</>}
-      {isInvitedDashboard && <>{/* TODO: 초대받은 대시보드 헤더 콘텐츠 */}</>}
+      <S.RightSection>
+        {isMyDashboard && <>{/* TODO: 마이 대시보드 헤더 콘텐츠 */}</>}
+        {isOwnedDashboard && <>{/* TODO: 내가 만든 대시보드 헤더 콘텐츠 */}</>}
+        {isInvitedDashboard && <>{/* TODO: 초대받은 대시보드 헤더 콘텐츠 */}</>}
+
+        <S.ProfileButton type="button" onClick={() => router.push('/mypage')}>
+          {user?.profileImageUrl ? (
+            <Image
+              src={user.profileImageUrl}
+              alt={user.nickname}
+              width={38}
+              height={38}
+              style={{ borderRadius: '50%', objectFit: 'cover' }}
+            />
+          ) : (
+            <S.ProfileFallback
+              style={{
+                background: user?.nickname
+                  ? getProfileColorByNickname(user.nickname)
+                  : '#E0E0E0',
+              }}
+            >
+              {user?.nickname?.[0] ?? '?'}
+            </S.ProfileFallback>
+          )}
+          <S.ProfileName>{user?.nickname}</S.ProfileName>
+        </S.ProfileButton>
+      </S.RightSection>
     </S.Wrapper>
   );
 }
